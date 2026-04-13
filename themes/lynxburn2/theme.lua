27e4e8920ae4476a6268d636f5c1d7f919a66207
anthrax <anthrax@lynxcore.org>
2026-04-13 19:00:52 +0200

    feature: [theme] space icons a bit

--- themes/lynxburn2/theme.lua
+++ themes/lynxburn2/theme.lua
@@ -207,10 +207,10 @@ theme.lxdisplay_osd_margin = 16
 theme.lxdisplay_osd_timeout = 1
 theme.lxdisplay_osd_screen_margin = 60
 
-theme.lxbluetooth_icon = ""
-theme.lxnetwork_icon = ""
-theme.lxpowerprofiles_icon_ac = ""
-theme.lxpowerprofiles_icon_battery = ""
+theme.lxbluetooth_icon = " "
+theme.lxnetwork_icon = " "
+theme.lxpowerprofiles_icon_ac = " "
+theme.lxpowerprofiles_icon_battery = " "
 
 theme.lxrunner_width = 520
 theme.lxrunner_row_count = 10
