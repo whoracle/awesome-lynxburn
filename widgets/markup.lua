93f242708ff91cc922176c209d9a476064ec30e6
anthrax <anthrax@lynxcore.org>
2026-05-04 09:00:26 +0200

    bugfix: [core] allow nested widget markup

--- widgets/markup.lua
+++ widgets/markup.lua
@@ -6,16 +6,20 @@ local function escape(text)
     return gears.string.xml_escape(tostring(text or ""))
 end
 
+local function content(text)
+    return tostring(text or "")
+end
+
 function M.color(color, text)
-    return string.format("<span foreground='%s'>%s</span>", escape(color), escape(text))
+    return string.format("<span foreground='%s'>%s</span>", escape(color), content(text))
 end
 
 function M.font(font, text)
-    return string.format("<span font_desc='%s'>%s</span>", escape(font), escape(text))
+    return string.format("<span font_desc='%s'>%s</span>", escape(font), content(text))
 end
 
 function M.bold(text)
-    return string.format("<b>%s</b>", escape(text))
+    return string.format("<b>%s</b>", content(text))
 end
 
 return setmetatable(M, {
